52c62585695b3f00f1ee62ac3809e55ae868abac
George <geo.rog09@gmail.com>
2020-10-20 01:23:23 +0100

    Dealer.deal() method

--- src/cardgame/Dealer.java
+++ src/cardgame/Dealer.java
@@ -1,26 +1,37 @@
-import java.util.ArrayList;
-
 public class Dealer {
 
     public Dealer(){}
 
     public static int[][] deal(){
-        int numberOfPlayers = 2;
+
         int[] pack = {14, 15, 2, 3, 4, 5, 1, 3, 553, 52, 5, 25, 2, 5, 25, 25};
+        int numberOfPlayers = 2;
+
         int[][] result = new int[numberOfPlayers*2][4];
+        int[] cards;
 
         for(int i = 0; i < numberOfPlayers; i++){
-            int[] temp = new int[4];
+            cards = new int[4];
             for(int j = 0; j < 4; j++){
-                temp[j] = pack[i+(j*numberOfPlayers)];
+                cards[j] = pack[i+(j*numberOfPlayers)];
             }
-            result[i] = temp;
+            result[i] = cards;
+        }
+
+        for(int i = numberOfPlayers*4; i < numberOfPlayers*5; i++){
+            cards = new int[4];
+            for(int j = 0; j < 4; j++){
+                cards[j] = pack[i+(j*numberOfPlayers)];
+            }
+            result[i - (numberOfPlayers*3)] = cards;
         }
 
         return result;
+
     }
 
     public static void main(String [] args){
-        int[][] results = deal();
+        deal();
     }
+
 }
