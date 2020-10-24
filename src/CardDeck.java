8340f4ac7cbd2379d90a63b94bb6a2ce179c0c46
George <geo.rog09@gmail.com>
2020-10-24 15:30:41 +0100

    addCard & removeCard

--- src/CardDeck.java
+++ src/CardDeck.java
@@ -3,6 +3,19 @@ import java.util.ArrayList;
 public class CardDeck {
     private ArrayList<Card> cards = new ArrayList<>();
 
-    public CardDeck() {
+    public CardDeck(int[] cardValues) {
+        for(int v:cardValues){
+            cards.add(new Card(v));
+        }
     }
+
+    public void addCard(Card c){
+        cards.add(c);
+    }
+
+    public Card removeCard(Card c){
+        cards.remove(c);
+        return c;
+    }
+
 }
