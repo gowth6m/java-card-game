3a36daacce7bfe075812899e29d61537db76e952
George <geo.rog09@gmail.com>
2020-10-24 15:36:15 +0100

    Add method doc comment

--- src/Dealer.java
+++ src/Dealer.java
@@ -2,6 +2,13 @@ public class Dealer {
 
     public Dealer(){}
 
+    /**
+     * Deals cards an array of card values to the specified number of players.
+     *
+     * @param pack Integer array input of card values from file. Should be 8 * numberOfPlayers.
+     * @param numberOfPlayers Number of players to deal to.
+     * @return [[[player1hand, player2hand, ...], [player1deck, player2deck, ...]]
+     */
     public static int[][][] deal(int[] pack, int numberOfPlayers){
 
         int[][][] result = new int[2][numberOfPlayers][4];
