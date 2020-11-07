bc0fab8460550578bcb1d150ed3bbca37d933e83
George <geo.rog09@gmail.com>
2020-11-07 22:22:32 +0000

    Remove unused imports

--- src/test/java/com/cardgame/TestCardGame.java
+++ src/test/java/com/cardgame/TestCardGame.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
 import java.net.URISyntaxException;
-import java.net.URL;
 
 public class TestCardGame {
     private final CardGame testGame = new CardGame(2, new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16});
