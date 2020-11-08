91680c2aaa013f1fedc83fc31ccb22de17a7ddaf
Gowtham <gowtham4n@gmail.com>
2020-11-08 16:41:58 +0000

    fixed the problem with test, added another file path option

--- src/main/java/cardgame/FileReader.java
+++ src/main/java/cardgame/FileReader.java
@@ -11,62 +11,77 @@ import java.util.Scanner;
 
 public class FileReader {
 
-    private final String name;
+    private final String fileName;
     private final List<String> listOfNumbers = new ArrayList<>();
     private final int numberOfPlayers;
     // File pathing for jar
     private final File rootForJar = new File(getClass().getProtectionDomain().getCodeSource().getLocation().toURI().getPath());
-    private String pathForJar = rootForJar.getParent();
+    private final String pathForJar = rootForJar.getParent();
     // File pathing for resource folder in main and test
-    private String pathToMainResource = Paths.get("src","main","resources").toFile().getAbsolutePath();
-    private String pathToTestResource = Paths.get("src","test","resources").toFile().getAbsolutePath();
+    private final String pathToMainResource = Paths.get("src", "main", "resources").toFile().getAbsolutePath();
+    private final String pathToTestResourceOne = Paths.get("src", "test", "resources").toFile().getAbsolutePath();
+    private final String pathToTestResourceTwo = Paths.get("resources").toFile().getAbsolutePath();
 
     /**
-     * Constructor for FileReader
-     * @param fileName name of the input pack by the player
+     * Class constructor specifying number of players and file name of the text file.
+     *
+     * @param numberOfPlayers the number of player playing the game
+     * @param fileName        name of input pack
+     * @throws URISyntaxException if invalid characters when trying to parse a string
      */
     public FileReader(int numberOfPlayers, String fileName) throws URISyntaxException {
         this.numberOfPlayers = numberOfPlayers;
-        name = fileName;
+        this.fileName = fileName;
     }
 
     /**
      * Gets the list of numbers from input pack.
-     * @return the list of numbers from the input pack as a list of Integers.
+     *
+     * @return the list of numbers from the input pack as a list of Integers
      */
     public int[] getListOfNumbers() {
         int[] numbers = new int[numberOfPlayers * 8];
-        for(int i = 0; i < listOfNumbers.size(); i++) {
+        for (int i = 0; i < listOfNumbers.size(); i++) {
             numbers[i] = Integer.parseInt(listOfNumbers.get(i));
         }
         return numbers;
     }
 
     /**
-     * Checks if text file exists in 3 different locations and sets the one where it exist as the path.
-     * @return file from either same path as jar file, main resource folder or test resource folder
+     * Checks if text file exists in four different locations and sets the one where it exist as the current root path
+     * and creates a file using that current root path and the name given when constructing FileReader.
+     * <p>
+     * This is to ensure that text file can be found when running this program as jar file or when running from
+     * IDE where main resource folder needs to be used instead or for JUnit tests where test resource folder is used.
+     *
+     * @return file created from either same path as jar file, main resource folder or test resource folder
      */
     public File fileLocator() {
         File file;
-        File fileForJar = new File(pathForJar,name);
-        File fileForMainResource = new File(pathToMainResource,name);
-        File fileForTestResource = new File(pathToTestResource,name);
+        File fileForJar = new File(pathForJar, fileName);
+        File fileForMainResource = new File(pathToMainResource, fileName);
+        File fileForTestResourceOne = new File(pathToTestResourceOne, fileName);
+        File fileForTestResourceTwo = new File(pathToTestResourceTwo, fileName);
         if (fileForJar.exists()) {
             file = fileForJar;
-        } else if (fileForMainResource.exists()){
+        } else if (fileForMainResource.exists()) {
             file = fileForMainResource;
+        } else if (fileForTestResourceOne.exists()) {
+            file = fileForTestResourceOne;
         } else {
-            file = fileForTestResource;
+            file = fileForTestResourceTwo;
         }
         return file;
     }
 
     /**
-     * Checks the file format, whether it has the right amount of numbers.
+     * Checks the file format - whether it has the right number of values according to the number of given players
+     * where the requirement of file has to have rows equal to 8 * number of players must be true.
+     *
      * @return - true if the file contains positive integers and the file has rows equal to 8 * number of players
      */
     public boolean readAndValidate() {
-        if (!fileLocator().exists()){
+        if (!fileLocator().exists()) {
             return false;
         }
         try {
@@ -75,12 +90,12 @@ public class FileReader {
                 listOfNumbers.add(myReader.next());
             }
             myReader.close();
-        } catch(IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        for(String number:listOfNumbers) {
-            try{
-                if(Integer.parseInt(number) < 0 || listOfNumbers.size() != 8 * numberOfPlayers){
+        for (String number : listOfNumbers) {
+            try {
+                if (Integer.parseInt(number) < 0 || listOfNumbers.size() != 8 * numberOfPlayers) {
                     return false;
                 }
             } catch (NumberFormatException ignored) {
@@ -92,13 +107,14 @@ public class FileReader {
 
     /**
      * Checks if the game is winnable by checking if there are 4 of the same cards in the input pack.
-     * @return true if game is winnable, false if not.
+     *
+     * @return true if game is winnable
      */
     public boolean isGameWinnable() {
-        for(int a:getListOfNumbers()) {
+        for (int a : getListOfNumbers()) {
             int counter = 0;
-            for(int b:getListOfNumbers()) {
-                if(a==b) {
+            for (int b : getListOfNumbers()) {
+                if (a == b) {
                     counter++;
                 }
             }
