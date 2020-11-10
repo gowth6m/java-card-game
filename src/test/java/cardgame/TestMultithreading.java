f33b0d759890509a775935893c2b4905edc7b1f2
Gowtham <gowtham4n@gmail.com>
2020-11-10 01:52:48 +0000

    change to testCase 5

--- src/test/java/cardgame/TestMultithreading.java
+++ src/test/java/cardgame/TestMultithreading.java
@@ -107,17 +107,17 @@ public class TestMultithreading {
      * TEST CASE 5: 10 player game
      * Player 9 should win the game as player 9 is assigned a hand of [9,9,19,29] at the start.
      * Only possibility of winning is by getting four 9s.
-     * Player 3's deck at start: [39,49,59,69]
+     * Player 3's deck at start: [39,9,59,69]
      */
     @Test
     public void testGameFive() {
         int[] pack = {
                 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
                 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-                11, 12, 13, 14, 15, 16, 17, 18, 9, 20,
+                11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
                 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
                 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-                41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
+                41, 42, 43, 44, 45, 46, 47, 48, 9, 50,
                 51, 52, 53, 54, 55, 56, 57, 9, 59, 60,
                 61, 62, 63, 64, 65, 66, 19, 68, 69, 70};
         final CardGame testGame5 = new CardGame(10, pack);
